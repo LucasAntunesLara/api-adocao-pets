78d6fffac16028b174a134333ff5ea018aa2d487
Lucas Antunes <lucasantuneslara06@gmail.com>
2026-06-11 10:23:55 -0300

    Corrigir endereço de rota de criação de novos usuários

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,6 +4,6 @@ const UserController = require('../controllers/userController')
 
 const router = express.Router()
 
-router.post('/register', UserController.create)
+router.post('/', UserController.create)
 
 module.exports = router
