22a3eb74601f281cfa5a28904929fc95522c4c73
Lucas Antunes <lucasantuneslara06@gmail.com>
2026-06-12 07:28:49 -0300

    Adicionar rotas de pets em app.js

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ const express = require('express')
 
 const authRoutes = require('./routes/authRoutes')
 const userRoutes = require('./routes/userRoutes')
+const petRoutes = require('./routes/petRoutes')
 
 const app = express()
 
 app.use(express.json())
 app.use('/', authRoutes)
 app.use('/users', userRoutes)
+app.use('/pets', petRoutes)
 
 module.exports = app
