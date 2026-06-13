d53a263e2a0016684934d463cd6f15fbfc2efb2e
Lucas Antunes <lucasantuneslara06@gmail.com>
2026-06-13 08:10:30 -0300

    Adicionar middleware de autenticação nas rotas de pets

--- src/routes/petRoutes.js
+++ src/routes/petRoutes.js
@@ -2,14 +2,15 @@ const express = require('express')
 const router = express.Router()
 
 const PetController = require('../controllers/petController')
+const {authenticateToken} = require('../middlewares/authMiddleware')
 
 //Públicas
 router.get('/available', PetController.getAvailable)
 //Admin
-router.get('/', PetController.getAll)
-router.get('/:id', PetController.getById)
-router.post('/', PetController.create)
-router.put('/:id', PetController.update)
-router.delete('/:id', PetController.delete)
+router.get('/', authenticateToken, PetController.getAll)
+router.get('/:id', authenticateToken, PetController.getById)
+router.post('/', authenticateToken, PetController.create)
+router.put('/:id', authenticateToken, PetController.update)
+router.delete('/:id', authenticateToken, PetController.delete)
 
 module.exports = router
