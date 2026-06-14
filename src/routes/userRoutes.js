dcc502e138bf520069284358fd3a6a2020c22e6d
Lucas Antunes <lucasantuneslara06@gmail.com>
2026-06-14 08:07:01 -0300

    [userRoutes.js] Adicionar rotas faltantes e incluir middleware de autenticação nas rotas que necessitam de autorização

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,26 @@
 const express = require('express')
-
+const {
+  authenticateToken,
+  authorizeRole,
+} = require('../middlewares/authMiddleware')
 const UserController = require('../controllers/userController')
 
 const router = express.Router()
 
 router.post('/', UserController.create)
+router.get(
+  '/',
+  authenticateToken,
+  authorizeRole('admin'),
+  UserController.findAll,
+)
+router.get('/:id', authenticateToken, UserController.findById)
+router.put('/:id', authenticateToken, UserController.update)
+router.delete(
+  '/:id',
+  authenticateToken,
+  authorizeRole('admin'),
+  UserController.delete,
+)
 
 module.exports = router
