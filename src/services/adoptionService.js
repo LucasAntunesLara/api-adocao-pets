919ffb437f93ec121b62a4339912335ce057ef36
Lucas Antunes <lucasantuneslara06@gmail.com>
2026-06-16 07:09:48 -0300

    Adicionar service de adoções

---
+++ src/services/adoptionService.js
@@ -0,0 +1,62 @@
+const AdoptionModel = require('../models/adoptionModel')
+const PetModel = require('../models/petModel')
+
+class AdoptionService {
+  static async getAllAdoptions(user) {
+    // return await AdoptionModel.findAll()
+    if (!user || user.role !== 'admin') {
+      const error = new Error(
+        'Acesso negado: apenas administradores podem visualizar o histórico de adoções.',
+      )
+      error.status = 403
+      throw error
+    }
+    return await AdoptionModel.findAll()
+  }
+
+  static async createAdoption(petId, user) {
+    if (!user || user.role !== 'adopter') {
+      const error = new Error(
+        'Acesso negado: apenas usuários com perfil "adopter" podem realizar adoções.',
+      )
+      error.status = 403
+      throw error
+    }
+
+    const pet = await PetModel.findById(petId)
+    if (!pet) {
+      const error = new Error('O pet informado não existe.')
+      error.status = 404
+      throw error
+    }
+
+    if (pet.status !== 'available') {
+      const error = new Error(
+        'Este pet não está disponível para adoção (já foi adotado).',
+      )
+      error.status = 400
+      throw error
+    }
+
+    const alreadyAdopted = await AdoptionModel.findExistingAdoption(
+      user.userId,
+      petId,
+    )
+
+    if (alreadyAdopted) {
+      const error = new Error('Você já adotou este pet anteriormente.')
+      error.status = 400
+      throw error
+    }
+
+    const adoptionId = await AdoptionModel.create(user.userId, petId)
+
+    return {
+      id: adoptionId,
+      userId: user.userId,
+      petId,
+      message: 'Adoção concluída com sucesso!',
+    }
+  }
+}
+module.exports = AdoptionService
