1df4be220d113c6ce1e95cbb02a4395b30c9922e
Lucas Antunes <lucasantuneslara06@gmail.com>
2026-06-16 09:43:07 -0300

    Adicionar script para executar migrations no banco de dados

---
+++ src/database/scripts/runMigrations.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-undef */
+const fs = require('fs')
+const path = require('path')
+require('dotenv').config()
+const {getConnection} = require('../connection')
+
+async function runMigrations() {
+  const connection = await getConnection({useDatabase: false})
+
+  try {
+    const scriptPath = path.join(__dirname, '../create_tables.sql')
+    const sqlScript = fs.readFileSync(scriptPath, 'utf8')
+
+    console.log(
+      '=============Executando script SQL de migração...=============',
+    )
+
+    await connection.query(sqlScript)
+
+    console.log(
+      '=============Banco e tabelas criados/atualizados com sucesso!=============',
+    )
+  } catch (error) {
+    console.error('Erro ao criar tabelas:', error)
+  } finally {
+    await connection.end()
+  }
+}
+module.exports = {
+  runMigrations,
+}
